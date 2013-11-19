58f0f3cdb65ee55989a15f3f50798a178d690d77
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2013-11-19 10:12:01 +0000

    Change output structure of pdf

--- tools/pdfify.sh
+++ tools/pdfify.sh
@@ -2,10 +2,12 @@ urlfile=$1
 cache="cache/docs"
 workingdocument='cache/htmlworkingdocument.html'
 outputdoc='pdf_output.pdf'
+targetfinaldoc="";
 
 for html_document in $(cat $urlfile | sed 's/.*www/govuk/g' )
 do
 	sourcedirectory=$(echo $html_document | sed 's/govuk/www/; s/\/[^\/]*$//'  )
+	targetfinaldoc=$(echo $sourcedirectory | sed 's/[^\.]*/pdf/' )
 	
 	echo "PDFIFYING $html_document"
 	cp $cache/$html_document/ordered_html.html $workingdocument
@@ -27,9 +29,9 @@ do
 #	wkhtmltopdf --minimum-font-size 2 -n -s A4 -B 12 -R 12 -L 12 -T 12 --no-background $cache/$sourcedirectory/htmlworkingdocument.html $cache/$outputdoc
 	cupsfilter -o size=A2 -o fit-to-page $cache/$sourcedirectory/htmlworkingdocument.html > $cache/$outputdoc
 	rm $sourcedirectory/htmlworkingdocument.html
-	mv $cache/$outputdoc $cache/$html_document/$outputdoc
-	echo $cache/$html_document/$outputdoc has been PDFified
-	open $cache/$html_document/$outputdoc 
+	mv $cache/$outputdoc $cache/$targetfinaldoc
+	echo $cache/$targetfinaldoc has been PDFified
+	open $cache/$targetfinaldoc 
 		
 done
 	
