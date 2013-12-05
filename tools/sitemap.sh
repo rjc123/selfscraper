86e8bd92cc20bbd1a5fe4db35e489a55e2612037
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2013-12-05 13:24:34 +0000

    Update sitemap for partials and whole sites

--- tools/sitemap.sh
+++ tools/sitemap.sh
@@ -2,7 +2,14 @@
 domain=$1
 subdir=$(echo $domain | sed 's/.*\///' )
 mkdir -p $domain
-wget --no-parent -I$subdir -r -l0 -k -x --delete-after --spider -t3 -o $domain.log $domain
+
+if [ -n $subdir ] 
+	then
+	wget --no-parent -r -l0 -k -x --delete-after --spider -t3 -o $domain.log $domain
+	else
+	wget --no-parent -I$subdir -r -l0 -k -x --delete-after --spider -t3 -o $domain.log $domain
+fi
+
 grep -F $domain $domain.log | grep -o 'http:\/\/[^\ \"\(\)\<\>]*' | sed 's/\/$//' | sort | uniq > $domain.urls.txt
 
 echo "DONE SPIDERING $domain"
