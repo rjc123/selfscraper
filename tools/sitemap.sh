2b5ab1d82228f26c6328970bd8dab8c1b79781a2
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2013-12-11 17:55:48 +0000

    Handle subdirs as well as main domains

--- tools/sitemap.sh
+++ tools/sitemap.sh
@@ -1,15 +1,16 @@
 
-domain=$1
-subdir=$(echo $domain | sed 's/.*\///' )
+target=$1
+domain=$(echo $target | sed 's/\/.*//g' )
+subdir=$(echo $target | sed 's/^[^\/]*//' | sed 's/[^\/]*\///' )
 mkdir -p $domain
 
 if [ $subdir ]
 	then
-	echo "Spidering a subdirectory $subdir"
-	wget --no-parent -I $subdir -r -l0 -k -x --delete-after --spider -t2 -o $domain.log $domain
+	echo "Spidering a subdirectory $domain/$subdir"
+	wget --no-parent -I $subdir -r -l0 -k -x --delete-after -t2 -o $domain.log $domain
 	else
-	echo "Spidering a whole domain, with $subdir as a subdir"
-	wget --no-parent -r -l0 -k -x --delete-after --spider -t2 -o $domain.log $domain
+	echo "Spidering a whole domain $domain"
+	wget --no-parent -r -l0 -k -x --delete-after -t2 -o $domain.log $domain
 fi
 
 grep -F $domain $domain.log | grep -o 'http:\/\/[^\ \"\(\)\<\>]*' | sed 's/\/$//' | sort | uniq > $domain.urls.txt
