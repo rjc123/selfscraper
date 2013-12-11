4dbb5b9795605d6acfd11a9184a08385ec9a7405
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2013-12-11 12:22:15 +0000

    Make subdirectory spidering work

--- tools/sitemap.sh
+++ tools/sitemap.sh
@@ -3,11 +3,13 @@ domain=$1
 subdir=$(echo $domain | sed 's/.*\///' )
 mkdir -p $domain
 
-if [ -n $subdir ] 
+if [ $subdir ]
 	then
-	wget --no-parent -r -l0 -k -x --delete-after --spider -t3 -o $domain.log $domain
+	echo "Spidering a subdirectory $subdir"
+	wget --no-parent -I $subdir -r -l0 -k -x --delete-after --spider -t2 -o $domain.log $domain
 	else
-	wget --no-parent -I$subdir -r -l0 -k -x --delete-after --spider -t3 -o $domain.log $domain
+	echo "Spidering a whole domain, with $subdir as a subdir"
+	wget --no-parent -r -l0 -k -x --delete-after --spider -t2 -o $domain.log $domain
 fi
 
 grep -F $domain $domain.log | grep -o 'http:\/\/[^\ \"\(\)\<\>]*' | sed 's/\/$//' | sort | uniq > $domain.urls.txt
