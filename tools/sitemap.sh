a9e2d94b81d490e041c2fdb2bf5ac9c38c86552d
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-01-20 11:48:00 +0000

    Ignore robots.txt in sitemapping

--- tools/sitemap.sh
+++ tools/sitemap.sh
@@ -7,11 +7,11 @@ mkdir -p $domain
 if [ $subdir ]
 	then
 	echo "Spidering a subdirectory $domain/$subdir"
-	wget --no-parent -I $subdir -r -l0 -k -x --delete-after -t2 -o $domain.$subdir.log $target
+	wget --no-parent -I $subdir -r -l0 -k -x --delete-after -t2 -e robots=off -o $domain.$subdir.log $target
 	grep -F $domain $domain.$subdir.log | grep -o 'http:\/\/[^\ \"\(\)\<\>]*' | sed 's/\/$//' | sort | uniq > $domain.$subdir.urls.txt
 	else
 	echo "Spidering a whole domain $domain"
-	wget --no-parent -r -l0 -k -x --delete-after -t2 -o $domain.log $target
+	wget --no-parent -r -l0 -k -x --delete-after -t2 -e robots=off -o $domain.log $target
 	grep -F $domain $domain.log | grep -o 'http:\/\/[^\ \"\(\)\<\>]*' | sed 's/\/$//' | sort | uniq > $domain.urls.txt
 fi
 
