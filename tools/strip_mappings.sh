00c98e95726c55e89a376c1f5bce820f0083c3cf
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-03-28 12:49:31 +0000

    Add strip mappings tool to clean junk urls

---
+++ tools/strip_mappings.sh
@@ -0,0 +1,10 @@
+
+# Usage sh tools/strip_mappings.sh target_file_to_strip
+
+mappings_file=$1
+
+for extension in $(cat tools/strip_list.txt);
+do
+    grep -v -F $extension $mappings_file > uniquetempfilename &&
+        mv uniquetempfilename $mappings_file
+done
